4f603886db1f256779b1a3294aff9471b0f734d1
HBK <sammcowen@gmail.com>
2022-01-25 22:16:58 -0800

    updated list-issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,5 +1,17 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+  
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+  };
 
 var getRepoIssues = function(repo) {
     //console.log(repo);
@@ -22,6 +34,16 @@ var getRepoIssues = function(repo) {
     if (issues.length === 0) {
         issueContainerEl.textContent = "This repo has no open issues!";
         return;
+      }
+      if (response.ok) {
+        response.json().then(function(data) {
+          displayIssues(data);
+      
+          // check if api has paginated issues
+          if (response.headers.get("Link")) {
+          displayWarning(repo);
+          }
+        });
       }
     for (var i = 0; i < issues.length; i++) {
         // create a link element to take users to the issue on github
